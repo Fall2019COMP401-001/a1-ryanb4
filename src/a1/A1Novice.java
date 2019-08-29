8fabe5c1442169e79d6b6c9fbc6d9fb4bf135233
ryanboals <ryanboals@172.17.99.47>
2019-08-29 10:34:33 -0400

    Added space after ":"

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -22,7 +22,7 @@ public class A1Novice {
 			String firstname = scan.next();
 			String lastname = scan.next();
 			System.out.print(firstname.charAt(0) + ".");
-			System.out.print(lastname + ":");
+			System.out.print(lastname + ": ");
 			
 			int count2 = scan.nextInt();
 			double total = 0;
