979399c346ed794b86ab2afb8f01545f021c384a
ryanboals <ryanboals@10.0.1.155>
2019-09-01 14:15:21 -0400

    Jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -9,114 +9,103 @@ public class A1Jedi {
 		Scanner scan = new Scanner(System.in);
 		
 		// Your code follows here.		
+
+		
 		//store inventory
 		
-		int numberofitems = scan.nextInt();
-		
-		String[] itemnames = new String[numberofitems];
-		Double[] prices = new Double[numberofitems];
-		
-		for (int i = 0; i < numberofitems; i++) {
-			
-			itemnames[i] = scan.next();
-			prices[i] = scan.nextDouble();
-			
-		}
-		
-		//number of customers
-		
-		int numberofcustomers = scan.nextInt();
-		
-		String[] name = new String[numberofcustomers];
-		Double[] totalexpenditures = new Double[numberofcustomers];
-		
-		
-		for (int j = 0; j < numberofcustomers; j++) {
-			
-			name[j] = scan.next() + " " + scan.next();
-			Double total = 0.00;
-			// number of items bought by customer
-			int customernumber = scan.nextInt();
-			
-			//items bought by customer loop
-			for (int k = 0; k < customernumber; k++) {
+				int numberofitems = scan.nextInt();
 				
-				int quantity = scan.nextInt();
-				String item = scan.next();
+				String[] itemnames = new String[numberofitems];
+				Double[] prices = new Double[numberofitems];
 				
-				//loop to see if item customer bought equals a store item
-				for (int l = 0; l < numberofitems; l++) {
+				for (int i = 0; i < numberofitems; i++) {
 					
-					if (itemnames[l].equals(item)) {
-						
-						
-						
-						Double temp = quantity * prices[l];
-						
-						total = total + temp;
-						totalexpenditures[j] = total;
-						
-						
-						
-						
-					
-					}
+					itemnames[i] = scan.next();
+					prices[i] = scan.nextDouble();
 					
 				}
 				
+				//number of customers
 				
-			}
+				int numberofcustomers = scan.nextInt();
+				
+				String[] name = new String[numberofcustomers];
+				Double[] totalexpenditures = new Double[numberofcustomers];
+				
+				int number[] = new int[numberofitems];
+				int itemamount[] = new int[numberofitems];
+				
+				
+				
+				for (int j = 0; j < numberofcustomers; j++) {
+					
+					name[j] = scan.next() + " " + scan.next();
+					
+					boolean samecustomer[] = new boolean[numberofitems];
+					
+					// number of items bought by customer
+					int customernumber = scan.nextInt();
+					
+					//items bought by customer loop
+					for (int k = 0; k < customernumber; k++) {
+						
+						int quantity = scan.nextInt();
+						String item = scan.next();
+						
+						//loop to see if item customer bought equals a store item
+						for (int l = 0; l < numberofitems; l++) {
+					
+							
+							if (itemnames[l].equals(item)) {
+								
+								itemamount[l] = itemamount[l] + quantity;
+								//System.out.println(itemamount[l] + itemnames[l]);
+								
+								
+								
+								if (!samecustomer[l]) {
+									number[l] = number[l] + 1;
+								}
+								
+								samecustomer[l] = true;
+								
+								
+								//System.out.println(number[l] + "customers");
+								
+								
+							}
+							
+						}
+						
+						
+					}	
+					
+					
+				
+					
+				}
+				
+				for (int x = 0; x < numberofitems; x++) {
+					
+					if (number[x] < 1) {
+						
+						System.out.println("No" + " customers bought " + itemnames[x]);
+						x++;
+					
+					} 
+					
+					
+					System.out.println(number[x] + " customers bought " + itemamount[x] + " " + itemnames[x]);
+					
 			
-			
-			
-			
-			
-			
-		
-			
-		}
-		
-		double max = 0.00;
-		String maxname = "";
-		for (int z = 0; z < numberofcustomers; z++) {
-			if (totalexpenditures[z] > max) {
-				max = totalexpenditures[z];
-				maxname = name[z];
-			}
-		}
-		
-		double min = max;
-		String minname = "";
-		for (int y = 0; y < numberofcustomers; y++) {
-			if (totalexpenditures[y] < min) {
-				min = totalexpenditures[y];
-				minname = name[y];
-			}
-		}
-		
-		double average = 0.00;
-		double temp2 = 0.00;
-		for (int x = 0; x < numberofcustomers; x++) {
-			temp2 = temp2 + totalexpenditures[x];
-			
-		}
-		average = temp2 / numberofcustomers;
-		
-		
-		
-		
-		
-		
-		
-		System.out.println("Biggest: " + maxname + " (" + String.format("%.2f", max) + ")" );
-		System.out.println("Smallest: " + minname + " (" + String.format("%.2f", min) + ")" );
-		System.out.println("Average: " + String.format("%.2f", average));
-		
-		
-		
-		
-		
-		
+				}
+				
+				
+				
+				
+				
+				
+				
 		
 		
 		
